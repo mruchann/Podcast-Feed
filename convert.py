41e2e58fc2704ebe646f719dfd398f3bdf266ae6
ruchan <ruchan.yavuzdemir@icloud.com>
2024-07-07 02:01:17 +0300

    Update convert.py and Add episodes.yaml

--- convert.py
+++ convert.py
@@ -13,17 +13,26 @@ def read_mp3_files():
                 audio_path = os.path.join('audio', file)
                 audio_file = eyed3.load(audio_path)
                 duration = time.strftime('%H:%M:%S', time.gmtime(audio_file.info.time_secs))
+                size = os.path.getsize(audio_path)
+                size_str = '{:,}'.format(size)
                 comments = ''
                 for comment in audio_file.tag.comments:
                     comments += comment.text
                 audio_files.append({
                     'title': audio_file.tag.title,
-                    'file': file,
+                    'description': comments,
+                    'file': '/audio/' + file,
                     'duration': duration,
-                    'comments': comments,
+                    'length': size_str,
                 })
             except Exception as e:
                 print(f'Error processing {audio_path}: {e}')
     return audio_files
 
-print(yaml.dump(read_mp3_files(), sort_keys=False))
+def convert_to_yaml():
+    data = read_mp3_files()
+    yaml_data = yaml.dump(read_mp3_files(), sort_keys=False)
+    with open('episodes.yaml', 'w') as file:
+        file.write(yaml_data)
+
+convert_to_yaml()
